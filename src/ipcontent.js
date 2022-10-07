5ef2f6f86d25bdb75445737484815b6bca88e775
Ivana <rubicode01@gmail.com>
2022-10-07 11:11:43 +0200

    create structure

---
+++ src/ipcontent.js
@@ -0,0 +1,167 @@
+const logo = "IP";
+const meta = {
+  title: "IP [Design+Code]",
+  description: "Design meets Code",
+};
+
+const datahome = {
+  title: "Design meets Code",
+  //   animated: {
+  //     first: "Design",
+  //     second: "meets",
+  //     third: "Code",
+  //   },
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataintro = {
+  title: "Intro",
+  description:
+    "Lange habe ich überlegt, wie ich diese Seite aufbauen soll und eines sei vorab gesagt, es war tatsächlich nicht so einfach das Richtige auszuwählen. Wie soll die Seite aussehen? Struktur, Bilder, Headlines, Farben, Typographie, etc. Wie sollte ich die Seite bennennen? Portfolio? Hat irgendwie jeder... einfach meinen Namen einfügen? Hat auch irgendwie jeder...Wieviel Effekte und Animationen sind gut? Es gibt so viele Dinge die mir durch den Kopf gegangen sind und Schlussendlich habe ich mich dazu entschlossen, es einfach zu halten...",
+};
+
+const dataabout = {
+  title: "I'M A DESIGNER",
+  description:
+    "VOM ALLTAG INSPIRIERT,UM NEUE NUTZERERLEBNISSE UND KREATIVE LÖSUNGEN ZU SCHAFFEN",
+  aboutme: "some text here about me",
+};
+
+const datatimeline = {
+  title: "Timeline",
+  description: "mein Werdegang",
+  timeline: [
+    {
+      id: 1,
+      title: "Full-Stack Web & App Entwickler",
+      experience: "Weiterbildung",
+      description:
+        "Frontend- und Backend Programmierung, Datenbanken (SQL, NoSQL), UI/UX Design, Agile Development",
+      // buttonText: "View Projects",
+      date: "2022",
+      icon: "school",
+    },
+    {
+      id: 2,
+      title: "Graphic Designer",
+      experience: "Berufserfahrung",
+      description:
+        "Visual Design, Creative Direction, Frontend-Developer, User Experience, Project Management",
+      // buttonText: "View Backend Projects",
+      date: "2004 - 2022",
+      icon: "work",
+    },
+    {
+      id: 3,
+      title: "Mediengestalterin Ditial und Printmedien",
+      experience: "Berufserfahrung",
+      description:
+        "Visual Design, User Experience, Project Management, Frontend-Development",
+      // buttonText: "Company Website",
+      date: "2022 - 2004",
+      icon: "work",
+    },
+    {
+      id: 4,
+      title: "Mediengestalterin Digital- und Printmedien",
+      experience: "Berufserfahrung",
+      description: "Visual Design, Project Management",
+      // buttonText: "Course Certificate",
+      date: "2001 - 2002",
+      icon: "work",
+    },
+    {
+      id: 5,
+      title: "Mediengestalter Digital- und Printmedien",
+      experience: "Ausbildung",
+      description: "Visual Design, Frontend-Development",
+      // buttonText: "College Projects",
+      date: "1998 - 2001",
+      icon: "school",
+    },
+  ],
+};
+
+const dataskills = [
+  {
+    name: "HTML",
+    value: 90,
+  },
+  {
+    name: "CSS",
+    value: 85,
+  },
+  {
+    name: "JavaScript",
+    value: 60,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "Graphic Design",
+    value: 95,
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  title: "Intersse Geweckt?",
+  subline: "ich würde mich freuen",
+  YOUR_EMAIL: "name@domain.com",
+  YOUR_FONE: "(555)123-4567",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const datasocial = {
+  github: "https://github.com",
+  linkedin: "https://linkedin.com",
+};
+
+export {
+  logo,
+  meta,
+  datahome,
+  dataintro,
+  dataabout,
+  datatimeline,
+  dataskills,
+  dataportfolio,
+  contactConfig,
+  datasocial,
+};
